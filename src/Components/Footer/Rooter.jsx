f34b2d944d972df953bd9e9f80f1ceb13025b5db
AhmedSami019 <easami019192@gmail.com>
2025-10-21 23:44:06 +0600

    root element added into router

---
+++ src/Components/Footer/Rooter.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Rooter = () => {
+    return (
+        <div>
+            <h1>this is footer</h1>
+        </div>
+    );
+};
+
+export default Rooter;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router';
+import Home from '../Pages/Home/Home';
+import Root from '../Pages/Root/Root';
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <p>this is a router</p>
+        component: Root,
+        children: [
+            {
+                index: true, 
+                path: "/",
+                component: Home
+            }
+        ]
     }
 ])
