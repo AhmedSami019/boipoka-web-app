dc19fa0d1e7849a00e62ac178c6264508ed878e8
AhmedSami019 <easami019192@gmail.com>
2025-10-22 20:04:21 +0600

    adding the card style

--- src/Components/Book/Book.jsx
+++ src/Components/Book/Book.jsx
@@ -1,23 +1,29 @@
 import React from "react";
 
-const Book = () => {
+const Book = ({book}) => {
+    console.log(book);
+
+    const {bookName, author, image, review, totalPages, rating, category, tags, publisher, yearOfPublishing} = book
+
   return (
     <div className="card bg-base-100 shadow-sm border-2 border-gray-200">
-      <figure className="p-5">
+      <figure className="p-5 m-5 bg-[#F3F3F3]">
         <img
-          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-          alt="Shoes"
-          className="rounded-xl"
+          src={image}
+          alt="book"
+          className="rounded-xl h-[180px]"
         />
       </figure>
-      <div className="card-body items-center text-center">
-        <h2 className="card-title">Card Title</h2>
-        <p>
-          A card component has a figure, a body part, and inside body there are
-          title and actions parts
-        </p>
+      <div className="card-body ">
+        <div className="flex gap-2">
+            {
+                tags.map(tag => <button className="bg-gray-100 px-4 py-1 rounded-md font-medium text-green-600">{tag}</button>)
+            }
+        </div>
+        <h2 className="card-title text-3xl font-bold">{bookName}</h2>
+        <p className="text-lg font-medium">by : {author}</p>
         <div className="card-actions">
-          <button className="btn btn-primary">Buy Now</button>
+          
         </div>
       </div>
     </div>
--- src/Components/Books/Books.jsx
+++ src/Components/Books/Books.jsx
@@ -7,7 +7,7 @@ const Books = ({books}) => {
             <h2 className='text-4xl font-bold text-center mb-5'>All Books</h2>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mb-10'>
                 {
-                    books.map(book => <Book key={book.bookId} books={books}></Book>)
+                    books.map(book => <Book key={book.bookId} book={book}></Book>)
                 }
             </div>
         </div>
