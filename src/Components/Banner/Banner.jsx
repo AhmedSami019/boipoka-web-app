78cb52fbed8fdd22185d1679a8865b3c3adcd0ec
AhmedSami019 <easami019192@gmail.com>
2025-10-22 19:09:50 +0600

    added the banner section in home page

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,21 +1,17 @@
 import React from "react";
+import bannerImage from "../../assets/books.jpg"
 
 const Banner = () => {
   return (
-    <div className="hero bg-base-200 ">
+    <div className="hero bg-base-300 my-10 rounded-2xl p-10">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <img
-          src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-          className="max-w-sm rounded-lg shadow-2xl"
+          src={bannerImage}
+          className=" rounded-lg shadow-2xl"
         />
-        <div>
-          <h1 className="text-5xl font-bold">Box Office News!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-          <button className="btn btn-primary">Get Started</button>
+        <div className="space-y-5 text-center lg:text-start">
+          <h1 className="text-5xl leading-14 font-extrabold">Books to freshen up your bookshelf</h1>
+          <button className="btn bg-[#03C755] text-white px-10 py-5">Get Started</button>
         </div>
       </div>
     </div>
