a05a198adc60c94f2c474be9fdd734ad84c4688a
ahmedsami019 <easami019192@gmail.com>
2025-10-23 19:13:18 +0600

    added readme.md file details

--- src/Components/Books/Books.jsx
+++ src/Components/Books/Books.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
-import Book from '../Book/Book';
+import React, { Suspense } from "react";
+import Book from "../Book/Book";
 
-const Books = ({books}) => {
-    const allBooks = books
-    return (
-        <div>
-            <h2 className='text-4xl font-bold text-center mb-5'>All Books</h2>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mb-10'>
-                {
-                    allBooks.map(book => <Book key={book.bookId} book={book}></Book>)
-                }
-            </div>
-        </div>
-    );
+const Books = ({ books }) => {
+  const allBooks = books;
+  return (
+    <div>
+      <h2 className="text-4xl font-bold text-center mb-5">All Books</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mb-10">
+        <Suspense fallback={<p>loading..........</p>}>
+          {allBooks.map((book) => (
+            <Book key={book.bookId} book={book}></Book>
+          ))}
+        </Suspense>
+      </div>
+    </div>
+  );
 };
 
 export default Books;
